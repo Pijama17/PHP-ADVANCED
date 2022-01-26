c65028bbac7c99205d31b58302741be2c392151f
Pijama17 <pijamafaze@gmail.com>
2022-01-26 22:33:58 +0100

    opd2

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+
+$persoon["voornaam"] = "Rayan";
+$persoon["achternaam"] = "Ouladomar";
+$persoon["leeftijd"] = 17;
+$persoon["opleiding"] = "software developer";
+
+echo $persoon['voornaam']. " ". $persoon['achternaam'] . " is " . $persoon['leeftijd']. " 17 jaar oud en ben een studen van rocva amstelland " . $persoon['opleiding'] ; 
+
+?>
+
+
+</body>
+</html>
