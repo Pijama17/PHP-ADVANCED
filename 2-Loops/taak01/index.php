95bc15b6e656e38fb4e55b2304a9f7d2bf1e80f9
Pijama17 <pijamafaze@gmail.com>
2022-01-26 22:37:00 +0100

    loop

---
+++ 2-Loops/taak01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+$leeftijd = 0;
+
+while( $leeftijd < 18){
+echo "ik ben"17 " " .$leeftijd. " jaar, ". " " ." dus ik kan nog niet stemmen,<br>";
+$leeftijd++;
+
+}
+
+?>
+</body>
+</html>
