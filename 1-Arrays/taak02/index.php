86e7cda928c2f01abc1ee005ea88251dff9ca538
Pijama17 <pijamafaze@gmail.com>
2022-02-07 12:37:05 +0100

    while loop

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -9,7 +9,7 @@ $persoon["achternaam"] = "Ouladomar";
 $persoon["leeftijd"] = 17;
 $persoon["opleiding"] = "software developer";
 
-echo $persoon['voornaam']. " ". $persoon['achternaam'] . " is " . $persoon['leeftijd']. " 17 jaar oud en ben een studen van rocva amstelland " . $persoon['opleiding'] ; 
+echo $persoon['voornaam']. " ". $persoon['achternaam'] . " is " . $persoon['leeftijd']. " 17 jaar oud en ben een studen van rocva amstelland/'' " . $persoon['opleiding'] ; 
 
 ?>
 
