215eb8d97347dc58a2bff2bdd763d157410d9ea1
Pijama17 <pijamafaze@gmail.com>
2022-02-11 13:56:25 +0100

    nummer

---
+++ 3-Forms/taak03/nummer.php
@@ -0,0 +1,7 @@
+<?php
+foreach($_GET as $key => $value){
+    echo $value . "<br>"; 
+}
+/*http://localhost/blok/3-Forms/taak03/nummer.php?artist=juice+wrld&titel=death+race&year=2019&genre=hiphop*/
+?>
+
