caaa7d50e33ab1f7b843b84966c563952a00b078
Pijama17 <pijamafaze@gmail.com>
2022-02-16 11:24:11 +0100

    eind

---
+++ opdracht/advanced-2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+<?php 
+echo "<body style='background-color:".$_POST['achtergrond']."'>";
+?>
+
+<?php
+    function maakRij($iets, $iets2){
+        echo "<tr>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['tekst']."; padding:".$_POST['padding']."px;'>$iets</td>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['tekst']."; padding:".$_POST['padding']."px;'>$iets2</td>";
+        echo "</tr>";
+    }
+
+       $info1 = array("key"=>"value", "naam"=>"Rayan Ouladomar abd rahimi", "leeftijd"=>"17", "klas"=>"1d");
+
+
+
+    echo "<table style='border:  ".$_POST['border-dikte']."px solid' width='300' cellspacing='0'>";
+
+
+foreach($info1 as $idk =>$idk2){
+    maakRij($idk, $idk2);
+}
+
+    echo "</table>"
+
+?>
+</body>
+</html>
