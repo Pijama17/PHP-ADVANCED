7f535f6455fd3ab7141de097e4a93e0b17c7239c
Pijama17 <pijamafaze@gmail.com>
2022-03-02 09:02:17 +0100

    functie 2

--- 4-Functions/taak02/functies_2.php
+++ 4-Functions/taak02/functies_2.php
@@ -4,23 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
-    
 <?php
-function rekenUit($num1, $num2, $num3){
-   $antwoord = $num1 + $num2 + $num3;
-   return $antwoord;
+function rekenUit($nummer1, $nummer2, $nummer3){
+    $antwoord = $nummer1 + $nummer2 + $nummer3;
+    return $antwoord;
 }
 
-$answer1 = rekenUit(3, 10, 10);
-$answer2 = rekenUit(4, 8, 1);
-$answer3 = rekenUit(3, 1, 2);
+echo "als ik de getallen 7,1 en 3 bij elkaar optel, is de uitkomst" . $answer1 = rekenUit(7, 1, 3). "<br>";
+echo "als ik de getallen 3,1 en 5 bij elkaar optel, is de uitkomst" . $answer2 = rekenUit(3, 1, 5). "<br>";
+echo "als ik de getallen 24,41 en 88 bij elkaar optel, is de uitkomst" . $answer3 = rekenUit(24, 41, 88). "<br>";
 
-echo $answer1 23 "<br>";
-echo $answer2 13 "<br>";
-echo $answer3 6 "<br>";
 ?>
 </body>
-</html>
+
